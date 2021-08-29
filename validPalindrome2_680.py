0589ffb62a8273efcd0086e5e573da07b797af9e
Plawan Rath <plawanrath@gmail.com>
2021-08-29 15:58:36 -0700

    leetcode 680

---
+++ validPalindrome2_680.py
@@ -0,0 +1,15 @@
+def validPalindrome(s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            return s[l:r] == s[l:r][::-1] or s[l+1:r+1] == s[l+1:r+1][::-1]
+    return True
+
+s1 = "abca"
+s2 = "abc"
+
+print(validPalindrome(s1))
+print(validPalindrome(s2))
